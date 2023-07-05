1a1f4b93d4dfc6b6e60768d7cc72849ad13b2a3d
janko12 <henrich.sury97@gmail.com>
2023-07-05 18:42:20 +0200

    Add form markup

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -1,7 +1,19 @@
 export default function BookForm() {
     return (
         <div className="form-container">
-            <form>form</form>
+            <span className="book-form-title">new book</span>
+            <form>
+                <input type="text" placeholder="title" />
+                <input type="text" placeholder="author" />
+                <input type="number" placeholder="pages" min="1" />
+                <div className="bottom-controls">
+                    <div className="read-check">
+                        <input type="checkbox" id="read-cb" />
+                        <label htmlFor="read-cb">read</label>
+                    </div>
+                    <button>add</button>
+                </div>
+            </form>
         </div>
     );
 }
