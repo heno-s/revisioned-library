1bfb5ddd113c1cb05ba592ababf03207cbc0ff45
janko12 <henrich.sury97@gmail.com>
2023-07-05 17:20:02 +0200

    Create Header markup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,13 @@
 export default function Header() {
-    return <div></div>;
+    return (
+        <header>
+            <h1 className="app-title">Book's Library</h1>
+            <div className="search">
+                <span>search:</span>
+                <div className="searchbox">
+                    <input type="text" placeholder="title" />
+                </div>
+            </div>
+        </header>
+    );
 }
