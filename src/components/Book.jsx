d6f9ee492fda7279f5fb08b6e376140b28413c55
janko12 <henrich.sury97@gmail.com>
2023-07-05 19:14:01 +0200

    Style delete and pages, change markup to button

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -2,14 +2,14 @@ export default function Book() {
     return (
         <div className="book">
             <div className="book-header">
-                <div className="book-status">read</div>
-                <div className="delete-book">x</div>
+                <button className="book-status">read</button>
+                <button className="delete-book">x</button>
             </div>
             <div className="book-body">
                 <span className="book-title">Harry Potter</span>
                 <span className="book-author">J. K. Rowling</span>
             </div>
-            <div className="book-footer">8</div>
+            <div className="book-pages">8</div>
         </div>
     );
 }
