237cac1e356b919415cc3afae61b3b87782d3bbd
janko12 <henrich.sury97@gmail.com>
2023-07-05 17:20:14 +0200

    Create Footer markup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faInstagram,
+    faFacebook,
+    faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+
 export default function Footer() {
-    return <div></div>;
+    return (
+        <footer>
+            <span>Contact: </span>
+            <div className="social-links">
+                <div className="github">
+                    <FontAwesomeIcon icon={faGithub} />
+                </div>
+                <div className="instagram">
+                    <FontAwesomeIcon icon={faInstagram} />
+                </div>
+                <div className="facebook">
+                    <FontAwesomeIcon icon={faFacebook} />
+                </div>
+            </div>
+        </footer>
+    );
 }
