2a2ca435b251e2a40cfee434034ea37977608d8b
janko12 <henrich.sury97@gmail.com>
2023-07-05 19:00:53 +0200

    Add Book markup

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,3 +1,15 @@
 export default function Book() {
-    return <div>Book</div>;
+    return (
+        <div className="book">
+            <div className="book-header">
+                <div className="book-status">READ</div>
+                <div className="delete-book">x</div>
+            </div>
+            <div className="book-body">
+                <span className="book-title">Harry Potter</span>
+                <span className="book-author">J. K. Rowling</span>
+            </div>
+            <div className="book-footer">8</div>
+        </div>
+    );
 }
