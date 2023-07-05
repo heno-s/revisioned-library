e28b3e64f1694542a2918931ebda536638c596ed
janko12 <henrich.sury97@gmail.com>
2023-07-05 19:31:02 +0200

    Add react controlled form

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -1,14 +1,58 @@
+import { useState } from "react";
+
 export default function BookForm() {
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [pages, setPages] = useState("");
+    const [isRead, setIsRead] = useState(false);
+
+    function handleTitleInput(evt) {
+        setTitle(evt.target.value);
+    }
+
+    function handleAuthorInput(evt) {
+        setAuthor(evt.target.value);
+    }
+
+    function handlePagesInput(evt) {
+        setPages(evt.target.value);
+    }
+
+    function handleStatusToggle(evt) {
+        setIsRead(evt.target.checked);
+    }
+
     return (
         <div className="form-container">
             <span className="book-form-title">new book</span>
             <form>
-                <input type="text" placeholder="title" />
-                <input type="text" placeholder="author" />
-                <input type="number" placeholder="pages" min="1" />
+                <input
+                    type="text"
+                    placeholder="title"
+                    value={title}
+                    onChange={handleTitleInput}
+                />
+                <input
+                    type="text"
+                    placeholder="author"
+                    value={author}
+                    onChange={handleAuthorInput}
+                />
+                <input
+                    type="number"
+                    placeholder="pages"
+                    min="1"
+                    value={pages}
+                    onChange={handlePagesInput}
+                />
                 <div className="bottom-controls">
                     <div className="read-check">
-                        <input type="checkbox" id="read-cb" />
+                        <input
+                            type="checkbox"
+                            id="read-cb"
+                            checked={isRead}
+                            onChange={handleStatusToggle}
+                        />
                         <label htmlFor="read-cb">read</label>
                     </div>
                     <button>add</button>
