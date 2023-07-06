9db1b335ba00ff7e1a1c2d359a895327e9280c6a
janko12 <henrich.sury97@gmail.com>
2023-07-06 16:42:16 +0200

    Change search box to be state controlled

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
 export default function Header() {
+    const [title, setTitle] = useState("");
+
+    function handleTitleChange(evt) {
+        setTitle(evt.target.value);
+    }
+
     return (
         <header>
             <h1 className="app-title">Book's Library</h1>
             <div className="search">
                 <span>search:</span>
                 <div className="searchbox">
-                    <input type="text" placeholder="title" />
+                    <input
+                        type="text"
+                        placeholder="title"
+                        value={title}
+                        onChange={handleTitleChange}
+                    />
                 </div>
             </div>
         </header>
